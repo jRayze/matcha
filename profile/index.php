74fba42458e7a18d880883712bbe1581e5939d90
Merlini <merlini.jayson@gmail.com>
2020-11-24 18:27:44 +0100

    profile page

---
+++ profile/index.php
@@ -0,0 +1,6 @@
+<?php
+    include ('../template/start.php');
+    include ('../template/header.php');
+    include ('profile.php');
+    include ('../template/end.php');
+?>
---
+++ profile/profile.php
@@ -0,0 +1,119 @@
+<body class="bodyProfile">
+    <div class="container-fluid" style="margin-top: 56px;">
+        <div class="row">
+            <div class="col-md-3 col-sm-12 py-2 d-flex align-items-center justify-content-center fixed-top"  id="left" >
+                <div class="card">
+                    <img class="card-img-top" src="../img/lachatteatamere.jpg" alt="Card image cap">
+                    <div class="card-body">
+                        <div class="nomAge">Ella Delachatte, 21 ans</div>
+                        <div class="genre">Hétérosexuel</div>
+                        <div class="rating" style="margin: auto; padding-bottom: 5px;">
+                            <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-star" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                                <path fill-rule="evenodd" d="M2.866 14.85c-.078.444.36.791.746.593l4.39-2.256 4.389 2.256c.386.198.824-.149.746-.592l-.83-4.73 3.523-3.356c.329-.314.158-.888-.283-.95l-4.898-.696L8.465.792a.513.513 0 0 0-.927 0L5.354 5.12l-4.898.696c-.441.062-.612.636-.283.95l3.523 3.356-.83 4.73zm4.905-2.767l-3.686 1.894.694-3.957a.565.565 0 0 0-.163-.505L1.71 6.745l4.052-.576a.525.525 0 0 0 .393-.288l1.847-3.658 1.846 3.658a.525.525 0 0 0 .393.288l4.052.575-2.906 2.77a.564.564 0 0 0-.163.506l.694 3.957-3.686-1.894a.503.503 0 0 0-.461 0z"/>
+                            </svg>
+                            <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-star" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                                <path fill-rule="evenodd" d="M2.866 14.85c-.078.444.36.791.746.593l4.39-2.256 4.389 2.256c.386.198.824-.149.746-.592l-.83-4.73 3.523-3.356c.329-.314.158-.888-.283-.95l-4.898-.696L8.465.792a.513.513 0 0 0-.927 0L5.354 5.12l-4.898.696c-.441.062-.612.636-.283.95l3.523 3.356-.83 4.73zm4.905-2.767l-3.686 1.894.694-3.957a.565.565 0 0 0-.163-.505L1.71 6.745l4.052-.576a.525.525 0 0 0 .393-.288l1.847-3.658 1.846 3.658a.525.525 0 0 0 .393.288l4.052.575-2.906 2.77a.564.564 0 0 0-.163.506l.694 3.957-3.686-1.894a.503.503 0 0 0-.461 0z"/>
+                            </svg>
+                            <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-star" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                                <path fill-rule="evenodd" d="M2.866 14.85c-.078.444.36.791.746.593l4.39-2.256 4.389 2.256c.386.198.824-.149.746-.592l-.83-4.73 3.523-3.356c.329-.314.158-.888-.283-.95l-4.898-.696L8.465.792a.513.513 0 0 0-.927 0L5.354 5.12l-4.898.696c-.441.062-.612.636-.283.95l3.523 3.356-.83 4.73zm4.905-2.767l-3.686 1.894.694-3.957a.565.565 0 0 0-.163-.505L1.71 6.745l4.052-.576a.525.525 0 0 0 .393-.288l1.847-3.658 1.846 3.658a.525.525 0 0 0 .393.288l4.052.575-2.906 2.77a.564.564 0 0 0-.163.506l.694 3.957-3.686-1.894a.503.503 0 0 0-.461 0z"/>
+                            </svg>
+                            <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-star" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                                <path fill-rule="evenodd" d="M2.866 14.85c-.078.444.36.791.746.593l4.39-2.256 4.389 2.256c.386.198.824-.149.746-.592l-.83-4.73 3.523-3.356c.329-.314.158-.888-.283-.95l-4.898-.696L8.465.792a.513.513 0 0 0-.927 0L5.354 5.12l-4.898.696c-.441.062-.612.636-.283.95l3.523 3.356-.83 4.73zm4.905-2.767l-3.686 1.894.694-3.957a.565.565 0 0 0-.163-.505L1.71 6.745l4.052-.576a.525.525 0 0 0 .393-.288l1.847-3.658 1.846 3.658a.525.525 0 0 0 .393.288l4.052.575-2.906 2.77a.564.564 0 0 0-.163.506l.694 3.957-3.686-1.894a.503.503 0 0 0-.461 0z"/>
+                            </svg>
+                            <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-star" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                                <path fill-rule="evenodd" d="M2.866 14.85c-.078.444.36.791.746.593l4.39-2.256 4.389 2.256c.386.198.824-.149.746-.592l-.83-4.73 3.523-3.356c.329-.314.158-.888-.283-.95l-4.898-.696L8.465.792a.513.513 0 0 0-.927 0L5.354 5.12l-4.898.696c-.441.062-.612.636-.283.95l3.523 3.356-.83 4.73zm4.905-2.767l-3.686 1.894.694-3.957a.565.565 0 0 0-.163-.505L1.71 6.745l4.052-.576a.525.525 0 0 0 .393-.288l1.847-3.658 1.846 3.658a.525.525 0 0 0 .393.288l4.052.575-2.906 2.77a.564.564 0 0 0-.163.506l.694 3.957-3.686-1.894a.503.503 0 0 0-.461 0z"/>
+                            </svg>
+                        </div>
+                        <p class="card-text" style="font-size: 15px;" >Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    </div>
+                    <ul class="list-group list-group-flush">
+                        <li class="list-group-item">
+                            <span class="badge badge-pill badge-primary">Sport</span>
+                            <span class="badge badge-pill badge-primary">Voyage</span>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+            <div class="col-md-9 offset-md-3 offset-sm-12 py-2" id="main" style='margin-top: 56px;'>
+                <form class="">
+                    <div class="form-row">
+                        <div class="col">
+                            <label for="prenom">Prénom</label>
+                            <input type="text" class="form-control" id="prenom" placeholder="First name">
+                        </div>
+                        <div class="col">
+                            <label for="nom">Nom</label>
+                            <input type="text" class="form-control" id="nom" placeholder="Last name">
+                        </div>
+                    </div>
+
+                    <div class="form-group">
+                        <label for="exampleInputEmail1">Email address</label>
+                        <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+                        <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+                    </div>
+                    <div class="form-group">
+                        <label for="exampleInputPassword1">Password</label>
+                        <input type="password" class="form-control" id="exampleInputPassword1">
+                    </div>
+                    <button type="submit" class="btn btn-primary">Submit</button>
+                </form>
+                <br >
+                <div style="border-bottom:solid 1px grey;"></div>
+                <br >
+                <form class="was-validated">
+                    <img src="../img/lachatteatamere.jpg" alt="..." class="img-thumbnail" style="width: 300px;">
+                    <img src="../img/lachatteatamere.jpg" alt="..." class="img-thumbnail" style="width: 200px;">
+                    <img src="../img/lachatteatamere.jpg" alt="..." class="img-thumbnail" style="width: 200px;">
+                    <img src="../img/lachatteatamere.jpg" alt="..." class="img-thumbnail" style="width: 200px;">
+                    <img src="../img/lachatteatamere.jpg" alt="..." class="img-thumbnail" style="width: 200px;">
+                    <div class="custom-file mb-3">
+                        <input type="file" class="custom-file-input" id="validatedCustomFile" required>
+                        <label class="custom-file-label" for="validatedCustomFile">Choose file...</label>
+                        <div class="invalid-feedback">Example invalid custom file feedback</div>
+                    </div>
+
+                    <label for="customControlValidation2" for="customControlValidation3" for="customControlValidation1">Orientation Sexuelle</label>
+                    <div class="custom-control custom-radio">
+                        <input type="radio" class="custom-control-input" id="customControlValidation2" name="radio-stacked" required>
+                        <label class="custom-control-label" for="customControlValidation2">Hétérosexuel</label>
+                    </div>
+                    <div class="custom-control custom-radio">
+                        <input type="radio" class="custom-control-input" id="customControlValidation3" name="radio-stacked" required>
+                        <label class="custom-control-label" for="customControlValidation3">Homosexuel</label>
+                    </div>
+                    <div class="custom-control custom-radio mb-3">
+                        <input type="radio" class="custom-control-input" id="customControlValidation1" name="radio-stacked" required>
+                        <label class="custom-control-label" for="customControlValidation1">Bisexuel</label>
+                        <div class="invalid-feedback">Selectionnez votre orientation sexuelle.</div>
+                    </div>
+
+                    <label for="custom-select">Genre</label>
+                    <div class="mb-3">
+                        <select class="custom-select" required>
+                        <option></option> 
+                        <option value="1">Homme</option>
+                        <option value="2">Femme</option>
+                        </select>
+                        <div class="invalid-feedback">Selectionnez votre genre.</div>
+                    </div>
+
+                    <div class="mb-3">
+                        <label for="validationTextarea">Bio</label>
+                        <textarea class="form-control is-invalid" id="validationTextarea" placeholder="Required example textarea" required></textarea>
+                        <div class="invalid-feedback">
+                        Ecrivez une petite biographie.
+                        </div>
+                    </div>
+                    <div class="mb-3">
+                        <label for="validationTextarea">Tags</label>
+                        <textarea class="form-control is-invalid" id="validationTextarea" placeholder="Required example textarea" required></textarea>
+                        <div class="invalid-feedback">
+                        Choisissez des tags.
+                        </div>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+</body>
