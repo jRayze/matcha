ee6f645172bd33f957bafebce0b9ab08b8cfd2c2
Bux42 <virgile.desvaux@gmail.com>
2020-12-06 09:45:20 +0100

    mark as seen at load & check if conversation belongs to user

--- php/chat/load_chat.php
+++ php/chat/load_chat.php
@@ -47,6 +47,12 @@ if (isset($_SESSION["user_id"])) {
         $chat_history_item["conv_id"] = $query["conv_id"];
         $chat_history_item["relative_date"] = get_relative_time($query["dateadded"]);
 
+        $stmt_unseen_count = $bdd->prepare("SELECT COUNT(*) FROM chat_messages WHERE seen=0 AND conv_id=$query[conv_id] AND from_user != $_SESSION[user_id]");
+        $stmt_unseen_count->execute();
+
+        $unseenQuery = $stmt_unseen_count->fetch();
+        $chat_history_item["unseen_count"] = $unseenQuery[0];
+
         $chat_history_item["msg"] = "";
         if ($query["id"] == $_SESSION["user_id"]) {
             $chat_history_item["msg"].= "Moi: ";
@@ -59,6 +65,9 @@ if (isset($_SESSION["user_id"])) {
         array_push($data["chat_history"], $chat_history_item);
     }
     if (isset($_SESSION["focus_chat_id"])) {
+        $stmt_set_as_read = $bdd->prepare("UPDATE chat_messages SET seen=1 WHERE conv_id=$_SESSION[focus_chat_id] AND from_user != $_SESSION[user_id];");
+        $stmt_set_as_read->execute();
+
         $data["focused_chat"]["conv_id"] = $_SESSION["focus_chat_id"];
         $stmt_focused_chat = $bdd->prepare("SELECT * FROM chat_messages WHERE conv_id=$_SESSION[focus_chat_id] ORDER BY dateadded ASC");
         $stmt_focused_chat->execute();
