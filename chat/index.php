9bc4e9137206fa2cf20bb476ca267e8502ccc665
Bux42 <virgile.desvaux@gmail.com>
2020-12-08 11:31:45 +0100

    redirect to login if not logged in

--- chat/index.php
+++ chat/index.php
@@ -1,5 +1,8 @@
 
 <?php
+if (!isset($_SESSION["user_id"])) {
+    header('Location: /login/login.php');
+}
     include ('../template/start.php');
     include ('../template/header.php');
     include ('chat2.php');
--- usersProfiles/index.php
+++ usersProfiles/index.php
@@ -1,4 +1,7 @@
 <?php
+if (!isset($_SESSION["user_id"])) {
+    header('Location: /login/login.php');
+}
     include ('../template/start.php');
     include ('../template/header.php');
     include ('userProfile.php');
