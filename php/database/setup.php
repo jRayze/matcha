386c351e9f232b182a7af6a0f2cfeeba32ace18e
Bux42 <virgile.desvaux@gmail.com>
2020-12-14 11:23:29 +0100

    updated table users

--- php/database/setup.php
+++ php/database/setup.php
@@ -2,15 +2,15 @@
 try {
     $conn = new PDO("mysql:host=localhost", "root", "");
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    $conn->exec("DROP DATABASE `matcha`;");
+    $conn->exec("DROP DATABASE IF EXISTS `matcha` ;");
 } catch (PDOException $e) {
-    echo $sql . "<br>" . $e->getMessage();
+    echo "<br>" . $e->getMessage();
 }
 try {
     $conn = new PDO("mysql:host=localhost", "root", "");
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     $conn->exec(file_get_contents("matcha.sql"));
 } catch(PDOException $e) {
-    echo $sql . "<br>" . $e->getMessage();
+    echo "<br>" . $e->getMessage();
 }
 ?>
